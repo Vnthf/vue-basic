e585423e7e33a2a92842146063b2b1718f0c8ae6
minhochoi <>
2017-10-25 12:04:40 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo: ''
+    },
+    methods: {
+      addTodo: function(){
+        this.newTodo = ''
+      }
+    }
+});
